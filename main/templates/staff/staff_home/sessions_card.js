bab388d889d6152da4167711887e76bd11f2e9fb
Jeffrey Kirchner <kirchner@chapman.edu>
2022-05-03 13:37:21 -0700

    sort sessions by date and title. fixes #61.

--- main/templates/staff/staff_home/sessions_card.js
+++ main/templates/staff/staff_home/sessions_card.js
@@ -23,3 +23,35 @@ sendDeleteSession(id){
     this.working = true;
     app.sendMessage("delete_session",{"id" : id});
 },
+
+
+/**
+ * sort sessions by title
+ */
+sortByTitle:function(){
+
+    app.working = true;
+
+    app.sessions.sort(function(a, b) {
+        a=a.title.trim().toLowerCase();
+        b=b.title.trim().toLowerCase();
+        return a < b ? -1 : a > b ? 1 : 0;
+    });
+
+    app.working = false;
+},
+
+/**
+ * sort sessions by date
+ */
+sortByDate:function(){
+
+    app.working = true;
+
+    app.sessions.sort(function(a, b) {
+        return new Date(b.start_date) - new Date(a.start_date);
+
+    });
+
+    app.working = false;
+},
