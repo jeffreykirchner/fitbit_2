7689c6cba743919e8016c7bfc915d64dd501bfc7
Jeffrey Kirchner <jkirchner@gmail.com>
2022-05-12 09:28:35 -0700

    remove jquery on ready. update login screen prevent submit.

--- main/forms/login_form.py
+++ main/forms/login_form.py
@@ -8,7 +8,8 @@ class LoginForm(forms.Form):
     '''
     log in form
     '''
-    username =  forms.EmailField(label='Email address (lower case)')
+    username =  forms.EmailField(label='Email address (lower case)',
+                                 widget=forms.TextInput(attrs={"v-on:keyup.enter":"login"}))
 
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput(attrs={"v-on:keyup.enter":"login"}))
