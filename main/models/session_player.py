cf2b8ea43b6436789c4120c57fa1c3dc4fd5b4cc
Jeffrey Kirchner <jkirchner@gmail.com>
2023-01-20 16:54:47 -0800

    add current average azm to server display. fixes #159.

--- main/models/session_player.py
+++ main/models/session_player.py
@@ -884,7 +884,8 @@ class SessionPlayer(models.Model):
 
             "todays_wrist_minutes" : todays_session_player_period.get_formated_wrist_minutes() if todays_session_player_period else "---",
             "todays_zone_minutes" :  todays_session_player_period.zone_minutes if todays_session_player_period else "---",
-
+             "todays_average_zone_minutes" :  todays_session_player_period.average_pay_block_zone_minutes if todays_session_player_period else "---",
+             
             #"survey_link" : self.get_current_survey_link(),
         }
     
@@ -940,6 +941,7 @@ class SessionPlayer(models.Model):
             "checked_in_today" : todays_session_player_period.check_in if todays_session_player_period else None,
             "todays_wrist_minutes" : todays_session_player_period.get_formated_wrist_minutes() if todays_session_player_period else "---",
             "todays_zone_minutes" :  todays_session_player_period.zone_minutes if todays_session_player_period else "---",
+            "todays_average_zone_minutes" :  todays_session_player_period.average_pay_block_zone_minutes if todays_session_player_period else "---",
         }
 
 
