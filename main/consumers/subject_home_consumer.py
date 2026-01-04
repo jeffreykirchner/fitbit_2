34f6f5a97023ba976f03b4c50f236ef15f74d4b0
Jeffrey Kirchner <jkirchner@gmail.com>
2026-01-04 23:47:51 +0000

    Enhance Fitbit integration: add error handling for multiple trackers and improve sync status messages. fixes #258.

--- main/consumers/subject_home_consumer.py
+++ main/consumers/subject_home_consumer.py
@@ -404,6 +404,8 @@ def take_get_session_subject(session_player_id, data):
                     fitbit_error_message = "Connect your fitbit the app."
                 elif value["message"] == "Not synced today":
                     fitbit_error_message = "Sync your fitbit to your phone and refresh."
+                elif value["message"] == "multiple fitbit trackers found":
+                    fitbit_error_message = "Multiple Fitbit trackers found, remove others from your account."
                 else:
                     fitbit_error_message = "Fitbit is not available, try again later."              
             else:
